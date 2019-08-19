b2b53a3c33475af1ceef775b6de1cdeb4f3fe0dc
koren <koren.hmel@gmail.com>
2019-08-19 14:30:40 +0300

    add url in case the image missing to index

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,6 @@
 class Link < ApplicationRecord
 	acts_as_votable
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>", thumb_plus: "200x200" }, default_url: "/images/:style/placeholder.png"
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>", thumb_plus: "200x200" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 	validates :title, :url, presence: true
 	belongs_to :user
