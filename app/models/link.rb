a4386c19c3a471edca98dbbd1ff13987612669f7
koren <koren.hmel@gmail.com>
2019-08-19 14:58:57 +0300

    change delete notice to index and show

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,7 +2,7 @@ class Link < ApplicationRecord
 	acts_as_votable
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>", thumb_plus: "200x200" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-	validates :title, :url, presence: true
+	validates :title, :url, :description,  presence: true
 	belongs_to :user
 	has_many :comments
 end
